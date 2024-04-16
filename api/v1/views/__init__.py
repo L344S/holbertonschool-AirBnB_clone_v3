82e65f702d754c4a2dd67564004c83dbc1ac6872
L344S <7474@holbertonstudents.com>
2024-04-16 17:56:59 +0200

    (Attempt to fix) Refactor import statements in api/v1/views/__init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,7 +4,13 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
+
+def register_views():
+    """Method that registers the views"""
+    import api.v1.views.index
+    import api.v1.views.states
+    import api.v1.views.cities
+    import api.v1.views.amenities
+
+
+register_views()
